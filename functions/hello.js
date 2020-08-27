8f55792bf3665141902663babb8b266d14660679
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-08-27 14:51:50 -0300

    Second commit

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,29 @@
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || "World"
+  const allProducts = [
+    {
+      "id": 1,
+      "name": "Camiseta",
+      "price": "99",
+      "quantity": "50",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "id": 2,
+      "name": "Calça Jeans",
+      "price": "150",
+      "quantity": "30",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "id": 3,
+      "name": "Allstar",
+      "price": "200",
+      "quantity": "10",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+  ]
   return {
     statusCode: 200,
-    body: `Hello ${subject}`
+    body: allProducts
   }
 }
