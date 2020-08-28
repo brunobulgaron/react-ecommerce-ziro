e10893af52221470273c24f23b1f4c20c2823419
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-08-28 01:19:23 -0300

    Reversing Firebase Tests

--- functions/getAllProducts.js
+++ functions/getAllProducts.js
@@ -1,66 +1,33 @@
 exports.handler = async event => {
-  import firebase from "firebase/app";
-  import "firebase/auth";
-  import "firebase/firestore";
-  console.log("oi");
-
-  const querySnapshot = await firebase.firestore().collection('products').get();
-
-  const products = querySnapshot.docs.map(doc => {
-    return {
-      id: doc.id,
-      ...doc.data()
-    }
-  });
-
-  // const allProducts = [
-  //   {
-  //     "id": 1,
-  //     "name": "Camiseta",
-  //     "price": "99",
-  //     "quantity": "50",
-  //     "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-  //   },
-  // ]
+  const allProducts = [
+    {
+      "id": 1,
+      "name": "Camiseta",
+      "price": "99",
+      "quantity": "50",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "id": 2,
+      "name": "Calça Jeans",
+      "price": "150",
+      "quantity": "30",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+    {
+      "id": 3,
+      "name": "Allstar",
+      "price": "200",
+      "quantity": "10",
+      "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+    },
+  ]
   return {
     statusCode: 200,
     headers: {
       "content-type":"application/json",
       "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify(products)
+    body: JSON.stringify(allProducts)
   }
 }
-// exports.handler = async event => {
-//   const allProducts = [
-//     {
-//       "id": 1,
-//       "name": "Camiseta",
-//       "price": "99",
-//       "quantity": "50",
-//       "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-//     },
-//     {
-//       "id": 2,
-//       "name": "Calça Jeans",
-//       "price": "150",
-//       "quantity": "30",
-//       "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-//     },
-//     {
-//       "id": 3,
-//       "name": "Allstar",
-//       "price": "200",
-//       "quantity": "10",
-//       "photo": "https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-//     },
-//   ]
-//   return {
-//     statusCode: 200,
-//     headers: {
-//       "content-type":"application/json",
-//       "Access-Control-Allow-Origin": "*"
-//     },
-//     body: JSON.stringify(allProducts)
-//   }
-// }
--- src/pages/AllProducts/AllProducts.js
+++ src/pages/AllProducts/AllProducts.js
@@ -8,66 +8,48 @@ import { ProductsListText, ProductsWrapper } from './style';
 function AllProducts() {
   const [product, setProduct] = useState([]);
 
-  // useEffect(() => {
-  //   const getProducts = async () => {
-  //     const querySnapshot = await firebase.firestore().collection('products').get();
-
-  //     const products = querySnapshot.docs.map(doc => {
-  //       return {
-  //         id: doc.id,
-  //         ...doc.data()
-  //       }
-  //     });
-
-  //     setProduct(products);
-  //   };
-
-  //   getProducts();
-  // }, [])
-
   useEffect(() => {
-    const getAllProducts = async () => {
-      const result = await axios('https://mystifying-chandrasekhar-4d2fb6.netlify.app/.netlify/functions/getAllProducts');
+    const getProducts = async () => {
+      const querySnapshot = await firebase.firestore().collection('products').get();
 
-      setProduct(result.data);
+      const products = querySnapshot.docs.map(doc => {
+        return {
+          id: doc.id,
+          ...doc.data()
+        }
+      });
+
+      setProduct(products);
     };
 
-    getAllProducts();
-  }, []);
+    getProducts();
+  }, [])
+
+  // useEffect(() => {
+  //   const getAllProducts = async () => {
+  //     const result = await axios('https://mystifying-chandrasekhar-4d2fb6.netlify.app/.netlify/functions/getAllProducts');
+
+  //     setProduct(result.data);
+  //   };
+
+  //   getAllProducts();
+  // }, []);
 
   return (
     <div>
-      <div>
-        <ProductsListText>Lista de Produtos</ProductsListText>
-        <ProductsWrapper>
-          {product.map(eachProduct => (
-            <ProductCard
-              key={eachProduct.id}
-              photo={eachProduct.photo}
-              price={eachProduct.price}
-              quantity={eachProduct.quantity}
-            />
-          ))}
-        </ProductsWrapper>
-      </div>
+      <ProductsListText>Lista de Produtos</ProductsListText>
+      <ProductsWrapper>
+        {product.map(eachProduct => (
+          <ProductCard
+            key={eachProduct.id}
+            photo={eachProduct.photo}
+            price={eachProduct.price}
+            quantity={eachProduct.quantity}
+          />
+        ))}
+      </ProductsWrapper>
     </div>
   )
-
-  // return (
-  //   <div>
-  //     <ProductsListText>Lista de Produtos</ProductsListText>
-  //     <ProductsWrapper>
-  //       {product.map(eachProduct => (
-  //         <ProductCard
-  //           key={eachProduct.id}
-  //           photo={eachProduct.photo}
-  //           price={eachProduct.price}
-  //           quantity={eachProduct.quantity}
-  //         />
-  //       ))}
-  //     </ProductsWrapper>
-  //   </div>
-  // )
 }
 
 export default AllProducts;
