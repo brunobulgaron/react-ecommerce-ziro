27c5cfcb9490db0fc3587e4ae17961df417fd85a
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-08-28 01:01:43 -0300

    Firebase testings

--- functions/getAllProducts.js
+++ functions/getAllProducts.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
 exports.handler = async event => {
   const querySnapshot = await firebase.firestore().collection('products').get();
 
