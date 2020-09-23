be9b63bf475d47693e85ae134f2a0ec640710b07
Bruno Bulgaron <brunohbulgaron@gmail.com>
2020-09-23 19:24:05 -0300

    Teste CEP

---
+++ functions/endpoints/searchCep.js
@@ -0,0 +1,25 @@
+exports.handler = async (event, context, callback) => {
+  if(event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "Content-Type"
+      }
+    };
+  };
+
+  const cepFrontEnd = JSON.parse(event.body);
+
+  const result = await get(`https://viacep.com.br/ws/${cepFrontEnd}/json/`);
+  
+  return callback(null, {
+    statusCode: 200,
+    headers: {
+      "content-type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify(result)
+  });
+}
